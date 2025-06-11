555edf9e7f168a6752aa2765b89b7333ea7455f6
Sire Bah <bahsire1@gmail.com>
2025-06-11 14:03:50 +0000

    initial projet

---
+++ src/test/java/Sire/tech/BasicCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package Sire.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
