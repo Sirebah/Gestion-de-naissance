9ef07b14a233a9bb295fca1d11ddf92da3fed43d
Sire Bah <sire.bah@webbfontaine.com>
2025-07-01 11:02:02 +0000

    Ajout de la base de données mysql et configuration manuelle de mon sql sans passé par auto-ddl

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO roles(role, description) values
+                                         ('ADMINISTRATOR', 'Ce rôle donne accès au profile de base '),
+                                         ('AGENT', 'Ce rôle donne un accès total à la plateforme '),
+                                         ('PUBLIC', 'Ce rôle donne accès au traitement de dossier des demandeurs');
